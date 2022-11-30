e8b9e0f57712aa0bce5f28dd7a79be5b588d7c0f
mileshenry31 <milesmhenry@icloud.com>
2022-11-29 16:07:28 -0800

    added a check digit

--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@ TODO:
     4. For it to be true once you add up all the digits (s) (10 - (s % 10)) mod 10 should equal check digit
 */
 #include <iostream>
+#include <array>
 
 // Just take away error and make u_long a thing
 typedef unsigned long u_long;
 bool luhns(std::string ccNum);
 int main() {
-    luhns("0298472");
+    luhns("01839187319137");
     return 0;
 }
 
 bool luhns(std::string ccNum) {
+    int checkDigit = ccNum.back() - '0';
     // create array the size of the string aka same number of "digits"
     int ccArr[ccNum.length()];
     // get length of array
@@ -24,7 +26,7 @@ bool luhns(std::string ccNum) {
     // iterate through the string and put each char in the array. has to be u_long or throws an error
     for (u_long i = 0; i < ccNum.length(); i++) {
         ccArr[i] = ccNum[i];
-        ccArr[i] = ccArr[i] - '0';
+        ccArr[i] = ccArr[i];
     }
     // go from right to left of array
     for (int i = len - 1; i >= 0; i -= 2) {
