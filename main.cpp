2f94f7fa4025b0e85bd7f37c21c680f756311944
mileshenry31 <milesmhenry@icloud.com>
2022-11-29 16:35:43 -0800

    removed check digit from number, updated todos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 /*
 TODO:
-    1. Drop first number of entered credit card number and save it as a check digit to use in TODO 4
-    2. Find all individual numbers that are 2 digits, split those digits, add them together, and put it back in the same spot the two digit number was
-    3. Add all of the resulting digits together
-    4. For it to be true once you add up all the digits (s) (10 - (s % 10)) mod 10 should equal check digit
+    1. Find all individual numbers that are 2 digits, split those digits, add them together, and put it back in the same spot the two digit number was
+    2. Add all of the resulting digits together
+    3. For it to be true once you add up all the digits (s) (10 - (s % 10)) mod 10 should equal check digit
 */
 #include <iostream>
 #include <array>
@@ -17,22 +16,33 @@ int main() {
 }
 
 bool luhns(std::string ccNum) {
-    int checkDigit = ccNum.back() - '0';
     // create array the size of the string aka same number of "digits"
-    int ccArr[ccNum.length()];
+    int ccArr[ccNum.length() - 1];
     // get length of array
     int len = sizeof(ccArr) / sizeof(ccArr[0]);
+    int checkDigit = ccNum.back() - '0';
     //char multipliedArr[len];
     // iterate through the string and put each char in the array. has to be u_long or throws an error
     for (u_long i = 0; i < ccNum.length(); i++) {
+       if (i == len) {
+            continue;
+        }
         ccArr[i] = ccNum[i];
         ccArr[i] = ccArr[i];
+        ccArr[i] = ccArr[i] - '0';
+        //std::cout << ccArr[i] << '\n';
+
     }
     // go from right to left of array
     for (int i = len - 1; i >= 0; i -= 2) {
+        /*if (i == len - 1) {
+            continue;
+        }*/
         ccArr[i] = ccArr[i] * 2;
         //std::cout << ccArr[i] << "\n";
     }
-    
+    for (u_long i = 0; i < len; i++) {
+        std::cout << ccArr[i] << ' ';
+    }
     return true;
 }
